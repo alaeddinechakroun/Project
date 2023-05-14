342116740161a2e7d4645a608f0043b15959b316
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2023-05-14 21:00:59 +0100

    updating the API controllers

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,7 @@
 const itemModel = require("../models/itemModels");
 
-// get items
+// @desc Get All Items
+// @route GET /api/items/get-item
 const getItemController = async (req, res) => {
   try {
     const items = await itemModel.find();
@@ -10,31 +11,34 @@ const getItemController = async (req, res) => {
   }
 };
 
-//add items
+// @desc Create New Item
+// @route POST /api/items/add-item
 const addItemController = async (req, res) => {
+  const itemId = req.params.itemId;
+  const newItem = new itemModel(req.body);
   try {
-    const newItem = new itemModel(req.body);
-    await newItem.save();
-    res.status(201).send("Item Created Successfully!");
+    const savedItem = await newItem.save()
+    try {
+      await itemModel.findOneAndUpdate(itemId, {
+        $push: {item: savedItem._id}
+      })
+    } catch (error) {
+      console.log(error)
+    }
+    res.status(200).json(savedItem)
   } catch (error) {
-    res.status(400).send("error", error);
-    console.log(error);
+    console.log(error)
   }
 };
 
-//update item
+// @desc Update One Item
+// @route PUT /api/items/edit-item/id
 const editItemController = async (req, res) => {
   try {
-    const { itemId } = req.body;
-    console.log(itemId);
-    await itemModel.findOneAndUpdate({ _id: itemId }, req.body, {
-      new: true,
-    });
-
-    res.status(201).json("item Updated");
+    const updateItem = await itemModel.findOneAndUpdate({_id:req.params.id}, { $set: req.body}, {new: true})
+    res.status(200).json(updateItem)
   } catch (error) {
-    res.status(400).send(error);
-    console.log(error);
+    console.log(error)
   }
 };
 
